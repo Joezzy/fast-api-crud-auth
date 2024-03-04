79eae3b4c6e40c4c850d56f3c17fe746c63a692f
joezzy <onalojoseph96@gmail.com>
2024-03-04 23:22:16 +0100

    Auth-db-fix-2

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ from config import settings
 
 
 # SQLALCHEMY_DATABASE_URL = f'{settings.DATABASE_URL}'
-SQLALCHEMY_DATABASE_URL = f'postgresql://root_joezzy:7i1GteVz51krwJdulNimlwb7FJzypuMP@dpg-cnj3om8l6cac739bqbig-a.oregon-postgres.render.com/fastapi_i719'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://root_joezzy:7i1GteVz51krwJdulNimlwb7FJzypuMP@dpg-cnj3om8l6cac739bqbig-a.oregon-postgres.render.com/fastapi_i719'
+SQLALCHEMY_DATABASE_URL = f'postgres://root_joezzy:7i1GteVz51krwJdulNimlwb7FJzypuMP@dpg-cnj3om8l6cac739bqbig-a/fastapi_i719'
 
 # SQLALCHEMY_DATABASE_URL = f'postgres://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}/{settings.DATABASE_NAME}'
 
