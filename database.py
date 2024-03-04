454f12a34f91c5f4aa843488c56c077abe283499
joezzy <onalojoseph96@gmail.com>
2024-03-04 23:17:09 +0100

    Auth-db-fix

--- database.py
+++ database.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f'{settings.DATABASE_URL}'
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}/{settings.DATABASE_NAME}'
+# SQLALCHEMY_DATABASE_URL = f'{settings.DATABASE_URL}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://root_joezzy:7i1GteVz51krwJdulNimlwb7FJzypuMP@dpg-cnj3om8l6cac739bqbig-a.oregon-postgres.render.com/fastapi_i719'
+
+# SQLALCHEMY_DATABASE_URL = f'postgres://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}/{settings.DATABASE_NAME}'
 
 
 engine = create_engine( SQLALCHEMY_DATABASE_URL)
